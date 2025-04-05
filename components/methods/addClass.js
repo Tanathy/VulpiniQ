e729c3f79d85f885e317f22131dcf668d03e5f50
tanathy <tanathy@gmail.com>
2025-04-05 21:33:11 +0200

    .

--- components/methods/addClass.js
+++ components/methods/addClass.js
@@ -1,9 +1,3 @@
-// Name: addClass
-// Method: Prototype
-// Desc: Adds one or more classes to each node, ignoring duplicates.
-// Type: Class Manipulation
-// Example: Q(selector).addClass("class1"); // Adds a single class <br> Q(selector).addClass("class1 class2"); // Adds multiple classes
-// Variables: classes, list, el
 Q.Ext('addClass', function (classes) {
     var list = classes.split(' '),
         nodes = this.nodes;
